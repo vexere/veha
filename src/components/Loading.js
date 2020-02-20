b6955e3dc7833423c759a76883ec98d7a1329ce5
Ngọc Nguyên <k15bhahaha@gmail.com>
2020-02-21 01:09:26 +0700

    Reformat just every single JS file using Prettier, with existing rules

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import { View, ActivityIndicator } from 'react-native'
-import { colors } from '../styles'
+import React, {Component} from 'react';
+import {View, ActivityIndicator} from 'react-native';
+import {colors} from '../styles';
+
 export default class Loading extends Component {
-    render() {
-        return (
-            <View style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignContent: 'center',
-                // alignItems: 'center',
-                // alignSelf: 'center',
-            }}>
-                <ActivityIndicator size="large" color={colors.blue} />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+          // alignItems: 'center',
+          // alignSelf: 'center',
+        }}>
+        <ActivityIndicator size="large" color={colors.blue} />
+      </View>
+    );
+  }
 }
